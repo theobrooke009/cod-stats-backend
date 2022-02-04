e3460d152148abc3fa7f5690cf1a1cfefdef0591
Theo Brooke <theobrooke009@gmail.com>
2022-02-04 23:17:19 +0000

    before free code camp method

--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const dbURI =
-  process.env.DB_URI || 'mongodb://127.0.0.1/warzone-three-db'
+export const dbURI = process.env.DB_URI || 'mongodb://127.0.0.1/warzone-three-db'
 
 export const port = process.env.PORT || 4000
 export const secret = process.env.SECRET || 'this is the secret, throw all your hands up'
